8133d89b2e892e9e888ace38a73b8d778e5f6c74
plaonn <plaonn@gmail.com>
2026-08-25 12:44:49 +0900

    add patch constants

---
+++ src/constants.js
@@ -0,0 +1,15 @@
+((OWP) => {
+  'use strict';
+
+  OWP.constants = Object.freeze({
+    SCRIPT_VERSION: '0.1.0',
+    ORCA_ENVIRONMENT_STORAGE_KEY: 'orca.web.runtimeEnvironment.v1',
+    PROFILE_STORAGE_KEY: 'orca.web.patches.runtimeProfile.v1',
+    RELOAD_GUARD_KEY: 'orca.web.patches.reloadGuard.v1',
+    CACHE_TTL_MS: 6 * 60 * 60 * 1000,
+    API_WAIT_TIMEOUT_MS: 15_000,
+    API_POLL_INTERVAL_MS: 100,
+    RUNTIME_CALL_TIMEOUT_MS: 15_000,
+    DEBUG_QUERY_PARAM: 'orcaWebPatchesDebug'
+  });
+})(globalThis.__OWP__);
